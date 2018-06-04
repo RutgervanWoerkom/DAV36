3a2a6c045b99986ce5557bae5a517906faaab883
Rutger van Woerkom <rutgervanwoerkom@gmail.com>
2018-06-04 15:33:38 +0200

    Met jaartal etc.

--- Code/PreProcessing.py
+++ Code/PreProcessing.py
@@ -5,6 +5,9 @@ clean = open('../Data/cleandata2018.csv','w')
 with open("../Data/2018unicode.csv") as f:
     reader = csv.reader(f)
     for row in reader:
-        if 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row or 'STRAATNAAM' in row:
-            clean.write(','.join(row))
-            clean.write('\n')
+        clean.write(','.join(row))
+        if 'STRAATNAAM' in row:
+            clean.write(',JAARTAL')
+        elif 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row:
+            clean.write(',2018')
+        clean.write('\n')
