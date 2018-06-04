356392f6440288bbce7a9ae3aefd88bf99f271f2
Rutger van Woerkom <rutgervanwoerkom@gmail.com>
2018-06-04 15:12:11 +0200

    met bovenste regel

--- Code/PreProcessing.py
+++ Code/PreProcessing.py
@@ -5,6 +5,6 @@ clean = open('../Data/cleandata2018.csv','w')
 with open("../Data/2018unicode.csv") as f:
     reader = csv.reader(f)
     for row in reader:
-        if 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row:
+        if 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row or 'STRAATNAAM' in row:
             clean.write(','.join(row))
             clean.write('\n')
