25b31195378f5f80c69ae026d25d63469a44407a
Jannes <jannes.roelink@gmail.com>
2018-06-04 14:38:24 +0200

    better csv file

--- Code/PreProcessing.py
+++ Code/PreProcessing.py
@@ -1,8 +1,6 @@
+import pandas as pd
 
-raw = open("..\\data\\2018Data.csv").read()
 
+#raw = open("..\\data\\unicode2018D.csv").read()
+pd.read_csv("..\\data\\2018unicode.csv")
 cleaned = open("..\\data\\cleandata2018.csv","w")
-
-
-cleaned.write("hello world")
-
