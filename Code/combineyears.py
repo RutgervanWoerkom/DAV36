72bc51b5f08fd927bd972e9db60e631627332d76
Jannes <jannes.roelink@gmail.com>
2018-06-06 11:57:26 +0200

    fiexd whitespaces

--- Code/combineyears.py
+++ Code/combineyears.py
@@ -5,30 +5,38 @@ clean = open('../Data/clean/combinedyears.csv','w')
 with open('../Data/clean/cleandata2018.csv') as f:
     reader = csv.reader(f)
     for row in reader:
-        clean.write(','.join(row))
-        clean.write('\n')
+        if 'STRAATNAAM' in row:
+            clean.write(','.join(row))
+            clean.write('\n')
+        elif 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row:
+            clean.write(','.join(row))
+            clean.write('\n')
 
 with open('../Data/clean/cleandata2017.csv') as f:
     reader = csv.reader(f)
     for row in reader:
-        clean.write(','.join(row))
-        clean.write('\n')
+        if 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row:
+            clean.write(','.join(row))
+            clean.write('\n')
     
 with open('../Data/clean/cleandata2016.csv') as f:
     reader = csv.reader(f)
     for row in reader:
-        clean.write(','.join(row))
-        clean.write('\n')
+        if 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row:
+            clean.write(','.join(row))
+            clean.write('\n')
 
 with open('../Data/clean/cleandata2015.csv') as f:
     reader = csv.reader(f)
     for row in reader:
-        clean.write(','.join(row))
-        clean.write('\n')
+        if 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row:
+            clean.write(','.join(row))
+            clean.write('\n')
         
 with open('../Data/clean/cleandata2014.csv') as f:
     reader = csv.reader(f)
     for row in reader:
-        clean.write(','.join(row))
-        clean.write('\n')
+        if 'AMSTERDAM' in row or 'AMSTERDAM ZUIDOOST' in row:
+            clean.write(','.join(row))
+            clean.write('\n')
 
