e121d4475ceff2590eb997abaca5ae6a0e20263e
Jannes <jannes.roelink@gmail.com>
2018-06-07 13:56:36 +0200

    texttobits

--- Code/texttobits.py
+++ Code/texttobits.py
@@ -11,4 +11,4 @@ df.loc[df.PRODUCTSOORT == 'ELK', 'PRODUCTSOORT'] = 0
 df.loc[df.PRODUCTSOORT == 'GAS', 'PRODUCTSOORT'] = 1
 
 
-df.to_csv('../Data/clean/Changedtobits.csv')
+df.to_csv('../Data/clean/Changedtobits.csv', index=False)
