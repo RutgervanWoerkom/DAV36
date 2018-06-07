b0f0644da289129ac97e4a95209f2dac81de29e9
Jannes <jannes.roelink@gmail.com>
2018-06-07 13:49:51 +0200

    texttobits

--- Code/removecollumn.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv('../Data/clean/combinedyears.csv')
-writer = open('../Data/clean/collumnsremoved.csv','w')
-
-df.drop(['MEETVERANTWOORDELIJKE', 'NETBEHEERDER', 'NETGEBIED', 'LANDCODE', 'WOONPLAATS', 'VERBRUIKSSEGMENT'], axis = 1, inplace = True)
-
-
-print(df)
---
+++ Code/texttobits.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df = pd.read_csv('../Data/clean/CombinedRemoved.csv')
+
+
+
+df.loc[df.MEETVERANTWOORDELIJKE != 'Liander N.V.', 'MEETVERANTWOORDELIJKE'] = 0
+df.loc[df.MEETVERANTWOORDELIJKE == 'Liander N.V.', 'MEETVERANTWOORDELIJKE'] = 1
+
+
+df.loc[df.PRODUCTSOORT == 'ELK', 'PRODUCTSOORT'] = 0
+df.loc[df.PRODUCTSOORT == 'GAS', 'PRODUCTSOORT'] = 1
+
+
+df.to_csv('../Data/clean/Changedtobits.csv')
